0c651815d4574b3307098149c6c2610c8bfabffb
Lucas DeRego <lucas.derego@gmail.com>
2014-03-09 10:59:59 -1000

    bullets move now

--- src/Systems/VelocitySystem.cpp
+++ src/Systems/VelocitySystem.cpp
@@ -1,12 +1,18 @@
 #include "Systems/VelocitySystem.h"
 
+#include <Fission/Rendering/Transform.h>
+
 #include "Components/Velocity.h"
 
 VelocitySystem::VelocitySystem(fsn::EntityManager& entityMgr) : fsn::ComponentSystem(entityMgr)
 {
-    mAspect.all<Velocity>();
+    mAspect.all<fsn::Transform, Velocity>();
 }
 
 void VelocitySystem::processEntity(const fsn::EntityRef& entity, const float dt)
 {
+    auto& transform = entity.getComponent<fsn::Transform>();
+    auto& vel = entity.getComponent<Velocity>();
+
+    transform.move(vel.x*dt, vel.y*dt);
 }
